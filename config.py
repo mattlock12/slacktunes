0c767699c7529db35b261f943493d7a8f44f73a6
mattlock12 <mattlock12@gmail.com>
2017-06-03 12:14:50 -0700

    be less dumb about config

--- config.py
+++ config.py
@@ -25,16 +25,20 @@ class ProdConfig(DevConfig):
             'SLACK_CLIENT_ID': os.environ['SLACK_CLIENT_ID'],
             'SLACK_CLIENT_SECRET': os.environ['SLACK_CLIENT_SECRET'],
             'SLACK_OAUTH_TOKEN': os.environ['SLACK_OAUTH_TOKEN'],
+            'SLACK_VERIFICATION_TOKEN': os.environ['SLACK_VERIFICATION_TOKEN'],
             'PLAYLIST_ID': os.environ['PLAYLIST_ID'],
             'YOUTUBE_CLIENT_ID': os.environ['YOUTUBE_CLIENT_ID'],
             'YOUTUBE_CLIENT_SECRET': os.environ['YOUTUBE_CLIENT_SECRET'],
-            'BASE_URL': 'https://slacktunes.me',
+            'BASE_URI': 'https://slacktunes.me',
             'CHANNEL_ID': '',  # TODO: change me
         }
 
 
 def get_config():
-    if getattr(os.environ, 'ENV', None) == PROD:
-        return ProdConfig().get_config()
+    try:
+        if os.environ.get('ENV') == PROD:
+            return ProdConfig().get_config()
+    except KeyError:
+        pass
 
     return DevConfig().get_config()
