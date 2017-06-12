8048eab16747c64e0630ee2d44bd2a334b15c4f8
mattlock12 <mattlock12@gmail.com>
2017-06-11 19:05:24 -0700

    wtf

--- application.py
+++ application.py
@@ -11,8 +11,7 @@ application.secret_key = str(uuid.uuid4())
 db = SQLAlchemy(application)
 db.create_all()
 
+from views import *
+
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=8000)
-
-import views
-
