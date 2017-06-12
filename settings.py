46b758abfe79959163b45f86fbf4b9fc2bbd3928
mattlock12 <mattlock12@gmail.com>
2017-06-11 20:30:33 -0700

    just use settings.py for settings

--- settings.py
+++ settings.py
@@ -1,42 +1,26 @@
-import os
-
-
 BASE_URI = 'https://slacktunes.me'
-try:
-    SLACK_CLIENT_ID = os.environ['SLACK_CLIENT_ID']
-    SLACK_CLIENT_SECRET = os.environ['SLACK_CLIENT_SECRET']
-    SLACK_OAUTH_TOKEN = os.environ['SLACK_OAUTH_TOKEN']
-    SLACK_VERIFICATION_TOKEN = os.environ['SLACK_VERIFICATION_TOKEN']
 
-    YOUTUBE_CLIENT_ID = os.environ['YOUTUBE_CLIENT_ID']
-    YOUTUBE_CLIENT_SECRET = os.environ['YOUTUBE_CLIENT_SECRET']
+SLACK_CLIENT_ID = ''
+SLACK_CLIENT_SECRET = ''
+SLACK_OAUTH_TOKEN = ''
+SLACK_VERIFICATION_TOKEN = ''
 
-    MYSQL_DB_FORMAT = 'mysql+pymysql://{username}:{password}@{server}:{port}/{db}'
-    MYSQL_USERNAME = os.environ['RDS_USER']
-    MYSQL_PASSWORD = os.environ['RDS_PASSWORD']
-    MYSQL_SERVER = os.environ['RDS_ENDPOINT']
-    MYSQL_PORT = os.environ['RDS_PORT']
-    MYSQL_DBNAME = os.environ['RDS_DBNAME']
+YOUTUBE_CLIENT_ID = ''
+YOUTUBE_CLIENT_SECRET = ''
 
-    DB_URI = MYSQL_DB_FORMAT.format(
-        username=MYSQL_USERNAME,
-        password=MYSQL_PASSWORD,
-        server=MYSQL_SERVER,
-        port=MYSQL_PORT,
-        db=MYSQL_DBNAME
-    )
+MYSQL_DB_FORMAT = 'mysql+pymysql://{username}:{password}@{server}:{port}/{db}'
+MYSQL_USERNAME = ''
+MYSQL_PASSWORD = ''
+MYSQL_SERVER = ''
+MYSQL_PORT = ''
+MYSQL_DBNAME = ''
 
-except KeyError:
-    pass
-
-try:
-    from local_settings import *
-except ImportError:
-    pass
-
-try:
-    YOUTUBE_CLIENT_ID
-except NameError as e:
-    raise NameError("######## LOOKS LIKE YOU FORGOT TO UPDATE LOCAL SETTINGS! #################")
+DB_URI = MYSQL_DB_FORMAT.format(
+    username=MYSQL_USERNAME,
+    password=MYSQL_PASSWORD,
+    server=MYSQL_SERVER,
+    port=MYSQL_PORT,
+    db=MYSQL_DBNAME
+)
 
 YOUTUBE_REDIRECT_URI = '%s/youtubeoauth2callback' % BASE_URI
