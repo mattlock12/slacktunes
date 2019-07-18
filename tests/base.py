2d24ba01b8fbb5eec37c4a12fa58eccf2afebdc6
Matt Leick <mattleick@MH-MBP-17-00036.local>
2019-07-17 21:37:31 -0500

    Add Some Tests
    update .gitignore
    
    create DatabaseTestCase as a base

---
+++ tests/base.py
@@ -0,0 +1,19 @@
+import os
+import unittest
+
+
+from app import application, db
+
+
+class DatabaseTestBase(unittest.TestCase):
+    def setUp(self):
+        application.config['TESTING'] = True
+        application.config['DEBUG'] = False
+        application.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
+        db.drop_all()
+        db.create_all()
+
+    def tearDown(self):
+        # TODO: idk what this does but far be it from me to disobey an SO answer
+        db.session.remove()
+        db.drop_all()
